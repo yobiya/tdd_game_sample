41bec0a0ffb4f8ec1982c503b7051feccf98a410
YoBiya <dummy@mail.com>
2014-01-19 22:47:04 +0900

    cocos2d-xのiOS用プロジェクトを追加
    UnitTestのビルド設定を追加

---
+++ cross/proj.ios/UnitTest/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  UnitTest
+//
+//  Created by YoBiya on 2014/01/19.
+//
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char * argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
